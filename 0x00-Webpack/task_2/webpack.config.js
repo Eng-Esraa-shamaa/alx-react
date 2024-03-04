f70841c0b2afc2e18b0029a56396b042942cafe2
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-03-04 18:11:51 +0200

    .

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -6,5 +6,29 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  performance: {
+    maxAssetSize: 100000,
+  },
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            }
+          }
+        ],
+      }
+    ],
+  },
 };
