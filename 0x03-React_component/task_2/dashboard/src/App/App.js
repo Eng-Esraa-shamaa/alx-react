13b9c248cec16642c55b41ecfa97cb2238cfd86f
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-03-13 17:35:43 +0200

    task 2

---
+++ 0x03-React_component/task_2/dashboard/src/App/App.js
@@ -0,0 +1,80 @@
+import React, {Component} from "react";
+import "./App.css";
+import Header from '../Header/Header';
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from 'prop-types';
+import { getLatestNotification } from "../utils/utils";
+
+
+
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 }
+
+];
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
+
+class App extends React.Component {
+  state = {  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+    };
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  handleKeyPress(e) {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+
+  render() { 
+    const { displayDrawer } = this.state;
+    const { isLoggedIn } = this.props;
+    return (
+      <React.Fragment>
+        <Notifications displayDrawer={displayDrawer} listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+ 
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {
+    return;
+  }
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+export default App;
