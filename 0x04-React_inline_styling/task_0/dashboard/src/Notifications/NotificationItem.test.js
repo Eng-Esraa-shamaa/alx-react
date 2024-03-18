a52375bc804d167b71bbfe692f0132e25b9adf9f
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-03-18 20:31:06 +0200

    task0 inline styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,37 @@
+import { shallow } from "enzyme";
+import React from "react";
+import NotificationItem from "./NotificationItem";
+import Notifications from "./Notifications";
+
+describe("<Notifications />", () => {
+  it("NotificationItem renders without crashing", () => {
+    const wrapper = shallow(<NotificationItem />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+
+  it("Verify that by passing dummy type and value props, it renders the correct html", () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    wrapper.update();
+    const listItem = wrapper.find("li");
+
+    expect(listItem).toHaveLength(1);
+    expect(listItem.text()).toEqual("test");
+    expect(listItem.prop("data-notification-type")).toEqual("default");
+  });
+
+
+  
+});
+describe("onclick event behaves as it should", () => {
+  it("should call console.log", () => {
+    const wrapper = shallow(<NotificationItem />);
+    const spy = jest.fn();
+
+    wrapper.setProps({ value: "test item", markAsRead: spy, id: 1 });
+    wrapper.find("li").props().onClick();
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(1);
+    spy.mockRestore();
+  });
+});
+
