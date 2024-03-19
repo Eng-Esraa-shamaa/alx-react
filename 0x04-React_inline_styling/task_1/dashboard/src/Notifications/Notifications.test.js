41b44617df91e51a46ba4b14e075f2343270a8f3
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-03-19 14:09:25 +0200

    .

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -15,7 +15,7 @@ afterEach(() => {
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: getLatestNotification() },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
 ];
 
 describe("Notification tests", () => {
@@ -25,16 +25,6 @@ describe("Notification tests", () => {
     expect(wrapper).toBeDefined();
   });
 
-  it("renders correct list items", () => {
-    const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
-    expect(wrapper.find("ul").children()).toHaveLength(listNotifications.length);
-    wrapper.find("ul").forEach((node) => {
-      expect(node.equals(<NotificationItem />));
-    });
-    expect(wrapper.find("ul").childAt(0).html()).toEqual('<li data-notification-type="default">New course available</li>');
-    expect(wrapper.find("ul").childAt(1).html()).toEqual('<li data-notification-type="urgent">New resume available</li>');
-    expect(wrapper.find("ul").childAt(2).html()).toEqual(`<li data-urgent=\"true\">${getLatestNotification()}</li>`);
-  });
 
   it("renders an unordered list", () => {
     const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
@@ -50,12 +40,6 @@ describe("Notification tests", () => {
     expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBe(false);
   });
 
-  // it("displays menu item when displayDrawer is false", () => {
-  //   const wrapper = shallow(<Notifications displayDrawer={false} />);
-
-  //   expect(wrapper.find("div.menuItem").exists()).toBe(true);
-  //   expect(wrapper.find("div.menuItem").html()).toEqual('<div class="menuItem"><p>Your notifications</p></div>');
-  // });
 
   it("does not display notifications when displayDrawer is false", () => {
     const wrapper = shallow(<Notifications displayDrawer={false} />);
